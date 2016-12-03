2b9b6a8d7d4e9337718169ef4e359eb35ae44414
Rafael Flores <rafael.flores@ensobusiness.com>
2016-12-03 14:45:01 -0400

    Changes in the messaging

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -3,7 +3,7 @@ class TwilioController < ApplicationController
 
  def voice
     response = Twilio::TwiML::Response.new do |r|
-      r.Say "Yay! Ninibeth thanks for calling,  this is for Popcorn too!", voice: "alice"
+      r.Say "Popcorn Bisou is time for grooming please tell your irresponsble mother to take you.thanks for calling, here is a soong for you.", voice: "alice"
       r.Sms "Well done building your first Twilio on Rails 5 app!"
       r.Play "http://linode.rabasa.com/cantina.mp3"
     end
